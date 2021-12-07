72004ca0c7e18073ef13a0ba7c52d813ed6f5817
Selene Grossett <selene_816@hotmail.com>
2021-12-07 03:26:39 +0000

    added new endpoint so that requests to the endpoint /u/:shortURL will redirect to its longURL

--- express_server.js
+++ express_server.js
@@ -42,14 +42,19 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.get("urls/new", (req, res) => {
   res.render("urls_new");
 });
 
 app.post("/urls", (req, res) => {
   console.log(req.body);
-  let shortURL = generateRandomString();
-  let longURL = req.body.longURL;
+  const shortURL = generateRandomString();
+  const longURL = req.body.longURL;
   urlDatabase[shortURL] = longURL;
   console.log(urlDatabase);
   res.redirect(`/urls/${shortURL}`);
