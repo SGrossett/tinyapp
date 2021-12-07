95612d38cb796676490517e10e87a66e9bec3590
Selene Grossett <selene_816@hotmail.com>
2021-12-07 02:20:25 +0000

    added a body parser and a post route to receive the form submission

--- express_server.js
+++ express_server.js
@@ -9,6 +9,9 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -41,5 +44,10 @@ app.get("/urls/:shortURL", (req, res) => {
 
 app.get("urls/new", (req, res) => {
   res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         
 });
 
